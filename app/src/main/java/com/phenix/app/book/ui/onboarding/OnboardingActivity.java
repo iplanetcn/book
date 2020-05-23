bf14703e47be2ea908f88150c8a72a925d21fd16
john <iplanetcn@gmail.com>
2020-05-23 23:30:46 +0800

    add splash,onboarding

---
+++ app/src/main/java/com/phenix/app/book/ui/onboarding/OnboardingActivity.java
@@ -0,0 +1,22 @@
+package com.phenix.app.book.ui.onboarding;
+
+import android.os.Bundle;
+
+import com.phenix.app.book.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * OnboardingActivity
+ *
+ * @author john
+ * @since 2020-05-23
+ */
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
---
+++ app/src/main/java/com/phenix/app/book/ui/splash/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.phenix.app.book.ui.splash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * SplashActivity
+ *
+ * @author john
+ * @since 2020-05-23
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
